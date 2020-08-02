95131f33b0796a28164f1c76ee860b61a3503cee
EC2 Default User <ec2-user@ip-172-31-28-228.us-east-2.compute.internal>
2020-08-02 11:41:49 +0000

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          helloowww thereeee!!!!
+          helloowww thereeee!!!! chnged in feature branch
         </p>
         <a
           className="App-link"
